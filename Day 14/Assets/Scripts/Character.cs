797a3446a7053d623e8ba1e277baca950a84d510
Shawn Baldwin <shawn@liftlockstudios.com>
2019-10-18 20:18:35 -0400

    Initial Day14 Instructor Resources Commit.

---
+++ Day 14/Assets/Scripts/Character.cs
@@ -0,0 +1,98 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Character : MonoBehaviour 
+{
+	public int maxHitPoints = 100;
+	public float deathTime = 3f;
+	public float hitReact = 0.1f;
+	private HealthBar healthBar;
+	private HealthMeter healthMeter;
+	private int currentHealth;
+	private Animator animController;
+	private float hitDelay;
+	private Transform agroTarget;
+	
+	// Use this for initialization
+	void Start () 
+	{
+		animController = GetComponent<Animator>();
+		currentHealth = maxHitPoints;
+		
+		healthBar = GetComponent<HealthBar>();
+		if(healthBar != null)
+		{
+			healthBar.SetHealth(currentHealth);	
+		}
+		
+		healthMeter = GetComponent<HealthMeter>();
+		if(healthMeter != null)
+		{
+			healthMeter.SetMaxHitPoints(currentHealth);	
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(hitDelay<=0)
+		{
+			animController.SetBool("tookDamage",false);
+		}
+		else
+		{
+			hitDelay-=Time.deltaTime;	
+		}
+		
+		if(currentHealth <= 0)
+		{
+			Die();	
+		}
+	}
+	
+	//The character has been hurt by something so apply the damage.
+	public void ApplyDamage(int amount)
+	{
+		
+		currentHealth -= amount;
+		if(currentHealth <= 0)
+		{
+			hitDelay=deathTime;
+			animController.SetBool("died",true);
+		}
+		else
+		{
+			hitDelay=hitReact;
+			animController.SetBool("tookDamage",true);
+		}
+		
+		if(healthBar != null)
+		{
+			healthBar.AlterHealth(-amount);	
+		}
+		
+		if(healthMeter != null)
+		{
+			healthMeter.AlterHealth(-amount);	
+		}
+	}
+	
+	private void Die()
+	{
+		if(hitDelay<=0)
+		{
+			Destroy(gameObject);
+		}
+	}
+	
+	public Transform GetAgroTarget()
+	{
+		return agroTarget ? agroTarget : null;	
+	}
+	
+	public void SetAgroTarget(Transform target)
+	{
+		agroTarget = target;
+	}
+	
+}
---
+++ Day 14/Assets/Scripts/Creator/SkinSelector.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SkinSelector : MonoBehaviour 
+{
+	public float skinValue = 0.5f;
+	public float skinTint = 0.05f;
+
+	void OnGUI()
+	{
+		GUI.Label(new Rect(510, 30, 90, 20), "Skin Shade:");
+		skinValue = GUI.HorizontalSlider(new Rect(585, 35, 256,20), skinValue, 0.4f, 1.0f);
+		GUI.Label(new Rect(510, 55, 90, 20), "Skin Tint:");
+		skinTint = GUI.HorizontalSlider(new Rect(585, 60, 256,20), skinTint, 0f, 0.1f);
+		if (GUI.Button(new Rect(550, 85, 80, 20), "Save Skin"))
+		{
+			PlayerPrefs.SetFloat("skinValue",skinValue);
+			PlayerPrefs.SetFloat("skinTint",skinTint);
+		}
+		
+		GetComponent<Renderer>().material.SetColor("_Color", new Color(skinValue+skinTint, skinValue+(skinTint+skinTint), skinValue, 1));
+	}
+}
