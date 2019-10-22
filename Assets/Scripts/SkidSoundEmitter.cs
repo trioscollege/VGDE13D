b1fc30d2557e2f3300b9f7f9262bb902716b406c
Shawn Baldwin <shawn@liftlockstudios.com>
2019-10-22 19:10:37 -0400

    Uploading project to Day06

---
+++ Assets/Scripts/SkidSoundEmitter.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(WheelCollider))]
+public class SkidSoundEmitter : MonoBehaviour 
+{
+	public float skidAt = 1.5f;
+	public int soundEmissionPerSecond = 10;
+	public AudioClip skidSound;
+	private float soundDelay;
+	private WheelCollider attachedWheel;
+	
+
+	// Use this for initialization
+	void Start () 
+	{
+		attachedWheel = transform.GetComponent<WheelCollider>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		WheelHit hit;
+		if(attachedWheel.GetGroundHit(out hit))
+		{
+			float frictionValue = Mathf.Abs(hit.sidewaysSlip);
+			if(skidAt <= frictionValue && soundDelay <= 0)
+			{
+				AudioSource.PlayClipAtPoint(skidSound,hit.point);
+				soundDelay = 1f;
+			}
+		}
+		
+		soundDelay-=Time.deltaTime*soundEmissionPerSecond;
+	}
+}
