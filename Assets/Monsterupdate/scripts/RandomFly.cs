053c1255fa83bcd3056fd24e228273bedb3f3309
Shawn Baldwin <shawn@liftlockstudios.com>
2019-11-03 05:04:54 -0500

    Updating Day09

---
+++ Assets/Monsterupdate/scripts/RandomFly.cs
@@ -0,0 +1,45 @@
+﻿// Converted from UnityScript to C# at http://www.M2H.nl/files/js_to_c.php - by Mike Hergaarden
+// Do test the code! You usually need to change a few small bits.
+
+using UnityEngine;
+using System.Collections;
+
+public class RandomFly : MonoBehaviour 
+{
+    public float speed = 5;
+    public float maxHeadingChange = 30;
+
+    private float heading;
+    private Vector3 targetRotation;
+
+    public AnimationClip flyAnimation;
+    public float flyAnimationSpeed = 1;
+
+
+    void Start() {
+        heading = Random.Range(0, 360);
+        transform.eulerAngles = new Vector3(0, heading, 0);
+        StartCoroutine(NewHeading());
+	}
+    
+    void Update() {
+        transform.eulerAngles = Vector3.Slerp(transform.eulerAngles, targetRotation, Time.deltaTime * 2);
+        transform.Translate(new Vector3(0,0,speed) * Time.deltaTime, Space.Self);
+        GetComponent<Animation>()[flyAnimation.name].speed = flyAnimationSpeed;
+        GetComponent<Animation>().CrossFade(flyAnimation.name, 0.2f);
+	}
+
+    IEnumerator NewHeading() {
+        while(true) {
+            NewHeadingRoutine();
+            yield return new WaitForSeconds(3);
+		}
+    }
+
+    void NewHeadingRoutine() {
+        float floor = Mathf.Clamp(heading - maxHeadingChange, 0, 360);
+        float ceil = Mathf.Clamp(heading + maxHeadingChange, 0, 360);
+        heading = Random.Range(floor, ceil);
+        targetRotation = new Vector3(0, heading, 0);
+    }
+}
---
+++ Assets/Scripts/Patroller.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Patroller : MonoBehaviour 
+{
+	public Transform[] route;
+	public float attackRadius = 2.1f;
+	public Collider weaponHitBox;
+	private Animator animController;
+	private UnityEngine.AI.NavMeshAgent agent;
+	private int currentPoint = 0;
+	private const float MIN_DISTANCE=3;
+	private Character character;
+	private int attackState;
+	private int deadState;
+	
+	
+	// Use this for initialization
+	void Start () 
+	{
+		agent = GetComponent<UnityEngine.AI.NavMeshAgent>(); 
+		character = GetComponent<Character>();
+		agent.SetDestination(route[currentPoint].position);
+		animController = GetComponent<Animator>();
+		attackState = Animator.StringToHash("UpperTorso.attack");
+		deadState = Animator.StringToHash("Base.die");
+		weaponHitBox.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		AnimatorStateInfo currentBaseState = animController.GetCurrentAnimatorStateInfo(0);
+		if(currentBaseState.fullPathHash != deadState)
+		{
+			Transform target = character.GetAgroTarget();
+			if(target != null)
+			{
+				agent.SetDestination(target.position);
+				Vector3 targetDirection = transform.position - target.position;
+				AnimatorStateInfo currentUpperTorsoState = animController.GetCurrentAnimatorStateInfo(1);
+				if(targetDirection.magnitude < attackRadius)
+				{				
+					if(currentUpperTorsoState.fullPathHash == attackState)
+					{
+						animController.SetBool("attacking",false);
+						weaponHitBox.enabled = true;
+					}
+					else
+					{
+						animController.SetBool("attacking",true);
+						weaponHitBox.enabled = false;
+					}
+				}
+				else
+				{
+					animController.SetBool("attacking",false);				
+					weaponHitBox.enabled = false;
+				}
+			}
+			else if((transform.position - route[currentPoint].position).magnitude < MIN_DISTANCE)
+			{	
+				++currentPoint;
+				
+				if(currentPoint >= route.Length)
+				{
+					currentPoint = 0;
+				}
+				
+				agent.SetDestination(route[currentPoint].position);
+			}
+		}
+	}
+}
