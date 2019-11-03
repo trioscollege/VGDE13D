416fb51bd2150d356e1eaa4b5e324dd4c44361d5
Shawn Baldwin <shawn@liftlockstudios.com>
2019-11-03 05:02:49 -0500

    Updating Day08

---
+++ Assets/Scripts/FollowTarget.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowTarget : MonoBehaviour 
+{
+	public Transform target;
+	private UnityEngine.AI.NavMeshAgent agent;
+
+	
+	// Use this for initialization
+	void Start () 
+	{
+		agent = GetComponent<UnityEngine.AI.NavMeshAgent>(); 
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		agent.SetDestination(target.position);
+	}
+}
