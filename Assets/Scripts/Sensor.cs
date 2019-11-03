b8f63832bffb1e3f084d88679d4d2191185533fd
Shawn Baldwin <shawn@liftlockstudios.com>
2019-11-03 04:59:47 -0500

    Updating Day07

---
+++ Assets/Scripts/Sensor.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sensor : MonoBehaviour 
+{
+	public WanderingMonster brainScript;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.tag == "Player")
+		{
+			brainScript.target = other.transform;	
+		}
+	}
+}
