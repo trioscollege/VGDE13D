d9284b3e8b5d9eb63c819fbce3d239dfe5c8caa8
100Vikings <jeff.gauvin@trios.com>
2021-02-04 01:42:04 -0500

    Begin ch4 updates.
    Add waypoint system.

---
+++ Assets/Scripts/Editor/WaypointEditor.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(WaypointGizmos))]
+public class WaypointEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        if (GUILayout.Button("Flatten Waypoints"))
+        {
+            WaypointGizmos wg = target as WaypointGizmos;
+            GameObject go = wg.gameObject;
+            Transform[] waypoints = go.GetComponentsInChildren<Transform>();
+
+            for (int i = 1; i < waypoints.Length; ++i)
+            {
+                Physics.Raycast(
+                    new Ray(
+                        waypoints[i].position + Vector3.up * 10000f,
+                        Vector3.down * float.MaxValue),
+                    out RaycastHit hit);
+
+                waypoints[i].position = 
+                    hit.point + Vector3.up * wg.m_flattenHeight;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/WaypointGizmos.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class WaypointGizmos : MonoBehaviour
+{
+    public float m_radius = 1.0f;
+    public Color m_color = Color.yellow;
+    [Range(1, 10)]
+    public int m_flattenHeight = 1;
+    private Transform[] m_waypoints;
+
+    void OnDrawGizmos()
+    {
+        m_waypoints = GetComponentsInChildren<Transform>();
+        Vector3 lastWaypoint =
+            m_waypoints[m_waypoints.Length - 1].position;
+
+        for (int i = 1; i < m_waypoints.Length; i++)
+        {
+            Gizmos.color = m_color;
+            Gizmos.DrawSphere(m_waypoints[i].position, m_radius);
+            Gizmos.DrawLine(lastWaypoint, m_waypoints[i].position);
+            lastWaypoint = m_waypoints[i].position;
+
+            m_waypoints[i].name = "Waypoint " + i.ToString();
+        }
+    }
+}
